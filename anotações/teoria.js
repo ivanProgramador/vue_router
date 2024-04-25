20fabc7669f2df6ec73c8778ffb2ea3148ae0989
ivan <ivan-guita@hotmail.com>
2024-04-25 20:09:38 -0300

    instalação da versão com o router

---
+++ anotações/teoria.js
@@ -0,0 +1,16 @@
+/*
+A bilbioteca Vue Router permite ter varias rotas dentro de uma mesma aplicação 
+sem precisar recarregar a pagina inteira, atenção nesse caso não basta apenas criar o app 
+existe uma comando especifico para criar ele com  router 
+
+npm create vue@latest
+
+essa comando vai fazer ele mostrar um questionario antes d fazer a instlação onde é possivel incluir
+a lib das rotas 
+
+
+
+
+
+
+*/
