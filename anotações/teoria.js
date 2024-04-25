b84f16349b1568f7319448757ea17659bef08cb9
ivan <ivan-guita@hotmail.com>
2024-04-25 20:34:33 -0300

    anatomia de uma rota

--- anotações/teoria.js
+++ anotações/teoria.js
@@ -6,7 +6,41 @@ existe uma comando especifico para criar ele com  router
 npm create vue@latest
 
 essa comando vai fazer ele mostrar um questionario antes d fazer a instlação onde é possivel incluir
-a lib das rotas 
+a lib das rotas.
+
+Criando um rota as rotas são criadas dentro do arquivo index que fica dentro da pasta routes, nesse 
+arquivo tem uma função chamada: createRouter que recebe um objeto como parametro esse objeto possui o array 
+routes onde cada indice dele é uma rota.
+
+um rota é um objeto que tem o path que eo caminho que da acesso a ela 
+o nome e por fim o Component que será renderizado quando essa rota for acessada 
+mas esse componete tem que ser importado para o arquivo antes de ser chamado.  
+
+ {
+      path: '/',
+      name: 'home',
+      component: HomeView
+ }
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
 
 
 
