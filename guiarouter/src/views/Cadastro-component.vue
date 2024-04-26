455c898e204f077eac4f94edb4883be0c566e417
ivan <ivan-guita@hotmail.com>
2024-04-25 21:36:37 -0300

    primeiro componete criado esta acessivel pela rota

---
+++ guiarouter/src/views/Cadastro-component.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <input type="text" name="email" id="email" placeholder="E-mail"/>
+      <input type="text" name="nome" id="nome" placeholder="nome"/>
+      <button>Teste !!!</button>
+    </div>
+    
+
+
+</template>
